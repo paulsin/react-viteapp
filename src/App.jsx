5f37e1a32202e5c0330c524cac188bd853432a97
paulsin <paulsin91@gmail.com>
2025-04-03 12:40:50 +0530

    image

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,75 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+ //import './App.css'
+import './custom/custom.css';
+import './custom/anju.css';
+import './custom/paulsin.css';
+import background from './images/background.jpg';
+import {
+  Route,
+  BrowserRouter,
+  Routes
+} from 'react-router-dom';
+ import Test from './components/accounts/Test';
+ import Home from './components/public/Home';
+//  import SignUp from './components/accounts/SignUp';
+
+//  import ListUsersCustomTable from './components/accounts/ListUsersCustomTable';
+//  import Login from './components/accounts/Login';
+//  import ProfileCheck from './components/accounts/ProfileCheck';
+//  import SignUpCheck from './components/accounts/SignUpCheck';
+//  import ListUsersCustomTableCheck from './components/accounts/ListUsersCustomTableCheck';
+//  import IndividualProperty from './components/public/IndividualProperty';
+//  import AboutAs from './components/public/AboutAs';
+// import ContactAs from './components/public/ContactAs';
+
+   import { SnackbarProvider, useSnackbar } from 'notistack';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+       {/* <h1>dfhhhhhhhhhhh</h1> */}
+        {/* <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a> */}
-        <h1>Haiii Paulsin.....haiii anju....reeve</h1>
-       </div>
+     {/* <h1>sdfvdxbgkkkk</h1> */}
+  
+     <SnackbarProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+          <Route path="/test" element={<Test />} />
+
+          {/* <Route path="/test2" element={<Test2 />} />
+          <Route path="/test3" element={<Test3 />} />*/}
+        {/* <Route path="/frontend/signupCheck" element={<SignUpCheck />} />
+          <Route path="/frontend/signupCheck/:newID" element={<SignUpCheck />} />
+          <Route path="/frontend/login" element={<Login />} />
+     
+          <Route path='/frontend/listusersowntable' element={<ListUsersCustomTableCheck />} />
+          <Route path='/frontend/profile' element={<ProfileCheck />} />  
+          <Route path="/frontend/individualProperty/:propertyID" element={<IndividualProperty />} />
+          <Route path="/frontend/aboutas" element={<AboutAs />} />
+          <Route path="/frontend/contactas" element={<ContactAs />} /> */}
+          {/* <Route path='/frontend/addProperty/:operation/:uniqueID' element={<AddPropertyCheck />} />
+          <Route path='/frontend/location/:locationType/:countryName' element={<LocationCheck />} />
+          <Route path='/frontend/location/:locationType/:countryName/:stateID' element={<LocationCheck />} />
+          <Route path='/frontend/location/:locationType/:countryName/:stateID/:districtID' element={<LocationCheck />} />
+          <Route path='/frontend/properties' element={<PropertiesCheck />} />
+          <Route path='/frontend/addimages/:propertyID' element={<AddImagesCheck />} />
+          <Route path="/frontend/individualProperty/:propertyID" element={<IndividualProperty />} />
+          <Route path="/frontend/aboutas" element={<AboutAs />} />
+          <Route path="/frontend/contactas" element={<ContactAs />} />
+          <Route path='/frontend/propertyCustomerRequestForOwner/:param1/:param2' element={<PropertyCustomerRequestForOwnerCheck />} />
+          <Route path='/frontend/listOwners' element={<OwnerCheck/>} /> */}
+
+        </Routes>
+      </BrowserRouter>
+    </SnackbarProvider>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/backend": {
+        target: "http://localhost:3000", // Proxy API requests to backend
+        changeOrigin: true,
+        secure: false,
+      },
+      optimizeDeps: {
+        include: ['react-bootstrap']
+      },
+      build: {
+        chunkSizeWarningLimit: 1000, // Increases limit from 500KB to 1000KB
+      },
+    },
+  },
+});
